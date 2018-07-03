a16e456056ad93b351107c36c5719fb35900e6ab
Luka Cosic <luka1cosic67@gmail.com>
2018-07-03 20:00:30 +0200

    Installed form validation

--- assets/fastfood/main.js
+++ assets/fastfood/main.js
@@ -28,5 +28,42 @@ $(document).ready(function() {
     });
   });
 
+  $("#maoform").validate({
+    rules: {
+        email: {
+            required: true,
+            email: true
+        },
+        name: {
+            required: true,
+            minlength: 2
+        },
+        phone: {
+            required: true
+        },
+        order: {
+            required: true,
+            minlength: 5
+        }
+    },
+    messages: {
+        email: {
+            required: '<p style="color: #f00;">Email adress is required</p>',
+            email: '<p style="color: #f00;">Please enter a valid email adress'
+        },
+        name: {
+            required: '<p style="color: #f00;">Name field is required</p>',
+            minlength: '<p style="color: #f00;">Name must have at least 2 characters</p>'
+        },
+        phone: {
+            required: '<p style="color: #f00;">Phone number is required</p>'
+        },
+        order: {
+            required: '<p style="color: #f00;">Order field is required</p>',
+            minlength: '<p style="color: #f00;">Order must have at least 5 characters</p>'
+        }
+    }
+});
+
 
 });
